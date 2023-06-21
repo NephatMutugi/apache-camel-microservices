f0ce757deaf39b3372589103f2286b01c9eb16e9
“NephatMutugi” <muchirinephat5@gmail.com>
2023-06-21 15:18:16 +0300

    transformations

--- camel-microservice-a/src/main/java/com/neph/microservices/camelmicroservicea/routes/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/neph/microservices/camelmicroservicea/routes/MyFirstTimerRouter.java
@@ -1,8 +1,35 @@
 package com.neph.microservices.camelmicroservicea.routes;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
 /**
- * @Author Nephat Muchiri
+ * @ Author Nephat Muchiri
  * Date 21/06/2023
  */
-public class MyFirstTimerRouter {
+@Component
+@RequiredArgsConstructor
+public class MyFirstTimerRouter extends RouteBuilder {
+    private final GetCurrentTimeBean getCurrentTimeBean;
+    @Override
+    public void configure() throws Exception {
+        // Create all routes here
+
+        // Starting point of routes
+        from("timer:first-timer")
+//                .transform().constant("Current Time "+ LocalDateTime.now())
+                .bean(getCurrentTimeBean, "getCurrentTime")
+                .to("log:first-timer");
+    }
+}
+
+// Bean to generate custom message
+@Component
+class GetCurrentTimeBean{
+    public String getCurrentTime(){
+        return "Time now is " + LocalDateTime.now();
+    }
 }
