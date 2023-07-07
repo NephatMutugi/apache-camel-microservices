a570f625fe5816d9be26977e7f06f4493e185402
“NephatMutugi” <muchirinephat5@gmail.com>
2023-07-07 20:45:18 +0300

    consume messages

--- camel-microservice-b/src/main/java/com/neph/microservices/camelmicroserviceb/configs/CamelMicroserviceConfigs.java
+++ camel-microservice-b/src/main/java/com/neph/microservices/camelmicroserviceb/configs/CamelMicroserviceConfigs.java
@@ -1,6 +1,18 @@
-package com.neph.microservices.camelmicroserviceb.configs;/**
+package com.neph.microservices.camelmicroserviceb.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
  * @ Author Nephat Muchiri
  * Date 07/07/2023
  */
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("activemq")
 public class CamelMicroserviceConfigs {
+    private String queueName;
 }
--- camel-microservice-b/src/main/java/com/neph/microservices/camelmicroserviceb/routes/ActiveMQReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/neph/microservices/camelmicroserviceb/routes/ActiveMQReceiverRouter.java
@@ -1,6 +1,21 @@
-package com.neph.microservices.camelmicroserviceb.routes;/**
+package com.neph.microservices.camelmicroserviceb.routes;
+
+import com.neph.microservices.camelmicroserviceb.configs.CamelMicroserviceConfigs;
+import lombok.RequiredArgsConstructor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
  * @ Author Nephat Muchiri
  * Date 07/07/2023
  */
-public class ActiveMQReceiverRouter {
+@Component
+@RequiredArgsConstructor
+public class ActiveMQReceiverRouter extends RouteBuilder {
+
+    private final CamelMicroserviceConfigs microserviceConfigs;
+    @Override
+    public void configure() throws Exception {
+        from("activemq:" + microserviceConfigs.getQueueName());
+    }
 }
